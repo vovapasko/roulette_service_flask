5f5081495f1e59e1f8a36960d87450ddd51ea122
unknown <vovapasko1699@gmail.com>
2019-12-12 23:23:46 +0200

    *

--- root/app.py
+++ root/app.py
@@ -9,14 +9,15 @@ app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-db = Database()
+# db = Database()
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = db.cstr
 
 
 # Route for handling the login page logic
 def get_users_log_pass():
-    users = db.fetchAllPlayers()
+    # users = db.fetchAllPlayers()
+    users = None
     users_data = []
     for user in users:
         user_dict = {}
