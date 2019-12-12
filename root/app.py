9abbadc6cd5afeace79f70024438adb03c6380b3
unknown <vovapasko1699@gmail.com>
2019-12-12 21:43:08 +0200

    a

---
+++ root/app.py
@@ -0,0 +1,19 @@
+# creating Flask instance variable app
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'it works!'
+
+
+@app.route('/<name>')
+def hello(name):
+    return 'it works! {0}'.format(name)
+
+
+if __name__ == '__main__':
+    app.run()
