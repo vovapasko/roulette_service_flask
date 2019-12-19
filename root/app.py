c12f5f8e3ec1d83197f9e6ebd2bd8a02a43e313a
vovapasko <vovapasko@gmail.com>
2019-12-19 21:57:50 +0200

    Provide logging

--- root/app.py
+++ root/app.py
@@ -15,7 +15,6 @@ app.config['SECRET_KEY'] = SECRET_KEY
 db = Database()
 app.config['SQLALCHEMY_DATABASE_URI'] = db.cstr
 
-
 # Route for handling the login page logic
 def get_users_log_pass():
     users_data = []
@@ -36,21 +35,29 @@ def welcome():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    print(session.get('username'))
+    print(f"at the beginning of login function {session.get('username')}")
     error = None
     if request.method == 'POST':
+        print(f"inside post method of login function")
+        print(session['username'])
         if request.form['username'] == 'admin' and request.form['password'] == 'admin':
             return redirect("https://super-app-150.herokuapp.com/")
         users_data = get_users_log_pass()
         for user_data in users_data:
+            print(f"inside post method of login function with users {session.get('username')}")
             user_login = user_data['player_id']
             user_passw = user_data['password']
             if request.form['username'] == user_login \
                     and request.form['password'] == user_passw:
+                print(f"inside post method of login function with users init complete {session.get('username')}")
+                print("Before saving data to session")
                 session['username'] = user_login
                 with db:
                     balance = db.fetchPlayer(user_login).balance
                     session['player_balance'] = balance
+                print("Saved data to session")
+                print(session.get('username'))
+                print(session.get('player_balance'))
                 return redirect('/home')
         error = 'Invalid Credentials. Please try again.'
     return render_template('login.html', error=error)
@@ -59,18 +66,21 @@ def login():
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     lst = generate_list()
-    username = session.get('username')
-    balance = session.get('player_balance')
-    print("In begin of home")
+    print(f"inside home function before game")
     print(session.get('username'))
     print(session.get('player_balance'))
+    username = session.get('username')
+    balance = session.get('player_balance')
     player = {'username': username, 'balance': balance}
     if request.method == 'POST':
         player_bet_money = request.form['moneyToBet']
+        print("inside post method home function. bet is done")
         print(username)
         print(balance)
+        print(session.get('username'))
+        print(session.get('player_balance'))
         if correct_bet(player_bet_money, balance):
-            print("In if")
+            print("Bet is correct. inside if correct bet")
             print(session.get('username'))
             print(session.get('player_balance'))
             bet = generate_bet()
@@ -84,6 +94,9 @@ def home():
             return render_template('play.html', lst=lst, player=player, bet=str(bet),
                                    player_bet=str(player_bet), bet_result=str(bet_result))
         else:
+            print("In else method. Bet is incorrect")
+            print(session.get('username'))
+            print(session.get('player_balance'))
             error = 'Wrong number! Bet must be number less than ' + str(balance)
             return render_template('play.html', lst=lst, player=player, error=error)
     print("In the end of home")
@@ -94,6 +107,9 @@ def home():
 
 @app.route('/logout')
 def logout():
+    print("In logout method")
+    print(session.get('username'))
+    print(session.get('player_balance'))
     username = session.get('username')
     new_balance = int(session.get('player_balance'))
     with db:
